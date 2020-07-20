39cf8be233a677878966148fc0b27a48a44c0474
cfhfelix <felix0523tw@gmail.com>
2020-07-20 18:34:04 +0800

    2020_0720

---
+++ test2/test2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test2
+//
+//  Created by 黃警鋒 on 2020/7/15.
+//  Copyright © 2020 黃警鋒. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
